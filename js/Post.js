8b8b446eba2e4162b7935409218d344c3c04f83e
Skultz <33635788+DNSKT@users.noreply.github.com>
2023-03-23 18:40:36 -0500

    Add files via upload

--- js/Post.js
+++ js/Post.js
@@ -2,18 +2,20 @@ export default {
     myPost(){
         let getPost = document.querySelector('#post');
         let posts = {};
+        let id = 0;
 
         getPost.addEventListener('submit', (e) => {
+            id += 1;
             e.preventDefault();
             let data = (Object.fromEntries(new FormData(e.target)));
             posts = data;
             console.log(posts);
             document.querySelector('.ps').insertAdjacentHTML('afterend', `<br>
-            <div class="container p-5 bg-dark rounded">
+            <div class="container p-4 bg-dark rounded card">
             
             <div class="card" style="width: 20rem;">
             <div class="card-body">
-                <h5 class="card-title">${posts.user}</h5>
+                <h5 class="card-title"><strong>${posts.user}</strong> ID: <span class="badge bg-success">${id}</span></h5>
                 <p class="card-text">${posts.message}</p>
             </div>
             </div>
